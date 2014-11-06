aca14107d558ffa255195d969631496617f01543
Yogi Ramchandani <yogiramchandani@gmail.com>
2014-11-06 10:24:08 +0000

    updated the nomination web grok parser

---
+++ runlogstash.bat
@@ -0,0 +1,4 @@
+REM D:\ELK\logstash-1.4.2\bin\logstash agent --debug -f D:/ELK/logstash-1.4.2/bin/logstash.conf -l D:/ELK/Data/logstash_logs/logs
+REM D:\ELK\logstash-1.4.2\bin\logstash agent --configtest --verbose -f D:/ELK/Data/logstash_config
+REM D:\ELK\logstash-1.4.2\bin\logstash agent --verbose -f D:/ELK/logstash-1.4.2/bin/logstash.conf -l D:/ELK/Data/logstash_logs/logs
+D:\ELK\logstash-1.4.2\bin\logstash agent -f D:/ELK/Data/logstash_config
